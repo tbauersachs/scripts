ec1113f4b53f432ab157115f42c61dc89bd74307
Todd <todd.bowersox@yahoo.com>
2016-07-15 17:56:39 -0700

    commit copyfiles

---
+++ copyfiles.ps1
@@ -0,0 +1,12 @@
+﻿Get-Content -path "C:\scripts\tas278results.txt" | foreach-object{ 
+
+if ($_ -like "*Web*") {
+write-host $_
+Copy-Item $_ C:\scripts\TAS-278\web\
+}
+write-host $_
+if ($_ -like "*Svc*") {
+Copy-Item $_ C:\scripts\TAS-278
+}
+
+}
