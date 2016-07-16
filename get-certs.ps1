a6a4f58dba378894763f57f502351129dad11623
Todd <todd.bowersox@yahoo.com>
2016-07-15 18:23:13 -0700

    added get-certs

---
+++ get-certs.ps1
@@ -0,0 +1,17 @@
+﻿
+
+
+$DaysToExpiration = 45
+$expirationDate = (Get-Date).AddDays($DaysToExpiration)
+
+
+
+$certs = Get-ChildItem CERT:localmachine/My |  ? {$_.NotAfter -lt $expirationDate} | select friendlyname, subject, @{Name="Expiration"; Expression= {$_.Notafter}} | fl | out-string
+
+
+if ($certs.Length -gt 1)
+{
+
+
+ Send-MailMessage -to "tbowersox@moneygram.com" -from "SSLalert@moneygram.com" -subject "[ALERT] Certs expiring within $DaysToExpiration days on $env:computername" -body $certs -SmtpServer "10.200.200.136" 
+}
